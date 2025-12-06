bb1bf4812bae04b491d2408c733a265e0914312e
Turgut Acar <turgtacar@gmail.com>
2025-12-06 13:16:18 +0300

    review

--- Services/Catalog/MultiShop.Catalog/Controllers/ProductSearchsController.cs
+++ Services/Catalog/MultiShop.Catalog/Controllers/ProductSearchsController.cs
@@ -21,7 +21,7 @@ namespace MultiShop.Catalog.Controllers
             string sortField = "productPrice",
             string sortOrder = "asc")
         {
-            //turgut acarrt
+            //turgut acarrtfghfghrr
             var response =  await _productSearchService.SearchProductsAsync(page, pageSize, sortField, sortOrder);
             return StatusCode(response.StatusCode, response);
 
